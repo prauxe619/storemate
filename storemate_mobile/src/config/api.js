fb7407ef5041fe27f72e6f79d1020acb7766bf9d
prauxe619 <connect.manim@gmail.com>
2026-08-13 15:27:46 +0530

    Update api.js

--- storemate_mobile/src/config/api.js
+++ storemate_mobile/src/config/api.js
@@ -5,3 +5,6 @@ const LOCAL_WIFI_IP = '192.168.31.65';
 
 // 📱 Default fallback URL
 export const BASE_URL = `http://${LOCAL_WIFI_IP}:5050`;
+
+export const API_BASE_URL =
+  'https://storemate-backend-production.up.railway.app';
